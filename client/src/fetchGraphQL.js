e2651c028628aec20967096325ae21952978dc1a
Abdirahman Jama <abdirahman.jama@thetrainline.com>
2022-06-26 13:38:09 +0100

    add helper function to fetch data from server

---
+++ client/src/fetchGraphQL.js
@@ -0,0 +1,16 @@
+import { text } from "stream/consumers";
+
+export default async function fetchGraphQL (text, variables) {
+   
+    const response = await fetch("http://localhost:4000/", {
+        method: "POST",
+        headers:{ "Content-Type":"application/json"},
+        body: JSON.stringify({
+            query: text, 
+            variables,
+        })
+
+    });
+    return await response.json();
+}
+
