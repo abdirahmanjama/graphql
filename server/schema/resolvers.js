ab7799153e0ed8e4fb3b760fa9afe60d273b1d7c
Abdirahman Jama <abdirahman.jama@thetrainline.com>
2022-06-26 11:51:00 +0100

    fix folder structure

---
+++ server/schema/resolvers.js
@@ -0,0 +1,55 @@
+const {UserList, MovieList} = require("../fakeData")
+const _ = require("lodash");
+
+const resolvers = {
+    Query: {
+        users: () => {
+            return UserList
+        },
+        //parent argument gives value resolved by parent in chain of types ??
+        user(parent, args) {
+            return _.find(UserList, {id: Number(args.id)})
+        },
+
+        movies: () => {
+            return MovieList
+        },
+
+        allowedMovie: (parent, args) => {
+            return _.find(MovieList, {familyFriendly: Boolean(args.familyFriendly)});
+        }
+    },
+
+    Mutation: {
+        createUser: (parent, args) => {
+            const user = args.input;
+            const lastUserID = UserList[UserList.length-1].id;
+            user.id = lastUserID + 1;
+            UserList.push(user);
+            console.log(user)
+            return user
+        },
+
+        updateUsername: (parent, args) => {
+        const { id, newUsername} = args.input;
+        let userUpdated; 
+        UserList.forEach(user => {
+            if(user.id === Number(id)){
+                user.username = newUsername;
+                userUpdated = user;
+            } 
+        })
+          return userUpdated;
+        },
+
+        //TODO: 
+        //given an ID -> remove user with ID from list
+        deleteUser: (parent, args) => {
+            const id = args.id;
+        }
+        
+        
+    }
+}
+
+module.exports = { resolvers }
---
+++ server/schema/typeDefs.js
@@ -0,0 +1,58 @@
+const { gql } = require("apollo-server");
+
+//Query is the first node of the graph.. 
+//Graph data structure is a collection of
+const typeDefs = gql`
+    type User {
+        id: ID!
+        name: String!,
+        username: String
+        age: Int! 
+        nationality: Nationality!
+        friends: [User]
+        favouriteMovies: [Movie] 
+    }
+
+    type Movie {
+        id: ID!
+        name: String!
+        description: String!
+        releaseYear: Int
+        familyFriendly: Boolean!
+    }
+
+    type Query {
+        users: [User!]!
+        user(id: ID!): User
+        movies: [Movie!]! 
+        allowedMovie(familyFriendly: Boolean!): Movie
+    }
+
+    input CreateUserInput {
+        name: String!,
+        username: String!
+        age: Int! 
+        nationality: Nationality = British
+    }
+
+    input UpdateUsernameInput {
+        id: ID! 
+        newUsername: String!
+    }
+
+    type Mutation {
+        createUser(input: CreateUserInput!): User
+        updateUsername(input: UpdateUsernameInput!): User
+        deleteUser(id: ID!): User
+    }
+
+    enum Nationality{
+        CANADA
+        BRAZIL
+        INDIA
+        GERMANY
+        CHILE
+    }
+`
+
+module.exports = {typeDefs}
